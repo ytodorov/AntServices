f67c1f400f9817a62b51fcebe04833a63be3cf7f
ytodorov <ytodorov@ytodorov.com>
2016-03-06 12:11:03 +0200

    string args2 = "-Pn -sn --unprivileged 8.8.8.8";

--- AdvancedNetToolsSolution/src/AdvancedNetToolsServicesWeb/Controllers/HomeController.cs
+++ AdvancedNetToolsSolution/src/AdvancedNetToolsServicesWeb/Controllers/HomeController.cs
@@ -71,7 +71,7 @@ namespace AdvancedNetToolsServicesWeb.Controllers
 
 
                 string path = abp + "\\nmap.exe";
-                string args2 = "8.8.8.8";
+                string args2 = "-Pn -sn --unprivileged 8.8.8.8";
                 Process p = new Process();
                 p.StartInfo.FileName = path;
                 p.StartInfo.Arguments = args2;
