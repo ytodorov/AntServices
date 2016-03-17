0cab9e9cb7938ebf25f18a6bf4c0a3bcd6052bc3
ytodorov <ytodorov@ytodorov.com>
2016-03-17 23:54:50 +0200

    added mvc6 to date

--- AdvancedNetToolsSolution/src/AdvancedNetToolsServicesWeb/Controllers/HomeController.cs
+++ AdvancedNetToolsSolution/src/AdvancedNetToolsServicesWeb/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ using System.Security.Cryptography;
 using System.Net.Sockets;
 using System.Diagnostics;
 using System.Threading;
+using System.IO;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -26,34 +27,40 @@ namespace AdvancedNetToolsServicesWeb.Controllers
             return "This is index";
         }
 
-        public string SendPing(string ip, int timeout = 1000, int bufferSize = 32, int ttl = 128, bool dontFragment = false)
+        public string Exec(string program, string args)
         {
+            Response.ContentType = "text/plain; charset=utf-8";
+            if (!program.EndsWith(".exe"))
+            {
+                program += ".exe";
+            }
+
             try
             {
-                PingOptions pingOptions = new PingOptions()
+                var allFiles = Directory.GetFiles(Startup.ApplicationBasePath, "*.*", SearchOption.AllDirectories);
+                var programFullPath = allFiles.FirstOrDefault(f => Path.GetFileName(f).ToLower().Equals(program.ToLower()));
+                if (!string.IsNullOrEmpty(programFullPath))
                 {
-                    Ttl = ttl,
-                    DontFragment = dontFragment
-                };
-                byte[] buffer = new byte[bufferSize];
-                RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
-                rand.GetBytes(buffer);
 
-                Ping ping = new Ping();
-                PingReply pr = ping.Send(ip, timeout, buffer, pingOptions);
+                    Process p = new Process();
+                    p.StartInfo.FileName = programFullPath;
+                    p.StartInfo.Arguments = args;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardError = true;
+                    p.StartInfo.UseShellExecute = false;
 
-                MyPingReply mpr = new MyPingReply()
-                {
-                    Address = pr.Address.ToString(),
-                    Buffer = pr.Buffer,
-                    Options = pr.Options,
-                    RoundtripTime = pr.RoundtripTime,
-                    Status = pr.Status
-                };
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.ErrorDialog = false;
 
-                string json = JsonConvert.SerializeObject(mpr, Formatting.Indented);
+                    p.Start();
+                    p.WaitForExit(10000);
 
-                return json;
+                    string result = p.StandardOutput.ReadToEnd();
+                    string error = p.StandardError.ReadToEnd();
+
+                    return result + "ГРЕШКИ" + error;
+                }
+                return "No such program " + program;
             }
             catch (Exception ex)
             {
@@ -61,181 +68,5 @@ namespace AdvancedNetToolsServicesWeb.Controllers
                 return json;
             }
         }
-
-        public string Tcp()
-        {
-            TcpClient client = new TcpClient();
-            client.Connect("8.8.8.8", 53);
-            return client.Connected.ToString();
-        }
-
-        public string Nmap()
-        {
-            try
-            {
-                var abp = Startup.ApplicationBasePath;
-                //C:\Projects\AdvancedNetToolsServicesRepo\AdvancedNetToolsSolution\src\AdvancedNetToolsServicesWeb\nmap.exe
-
-
-                string path = abp + "\\psping.exe";
-                string args2 = " -i 0 -n 10 8.8.8.8:53";
-                Process p = new Process();
-                p.StartInfo.FileName = path;
-                p.StartInfo.Arguments = args2;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.RedirectStandardError = true;
-                p.StartInfo.UseShellExecute = false;
-                p.Start();
-
-                //Thread.Sleep(10000);
-
-                //string result = "";
-                string result = p.StandardOutput.ReadToEnd();
-                string error = p.StandardError.ReadToEnd();
-                return result + "ГРЕШКИ" + error;
-            }
-            catch (Exception ex)
-            {
-                string json = JsonConvert.SerializeObject(ex, Formatting.Indented);
-                return json;
-            }
-        }
-
-        public string Whois()
-        {
-            try
-            {
-                var abp = Startup.ApplicationBasePath;
-                //C:\Projects\AdvancedNetToolsServicesRepo\AdvancedNetToolsSolution\src\AdvancedNetToolsServicesWeb\nmap.exe
-
-
-                string path = abp + "\\whois.exe";
-                string args2 = " 8.8.8.8";
-                Process p = new Process();
-                p.StartInfo.FileName = path;
-                p.StartInfo.Arguments = args2;
-                p.StartInfo.RedirectStandardOutput = true;
-                p.StartInfo.RedirectStandardError = true;
-                p.StartInfo.UseShellExecute = false;
-                p.Start();
-
-                //Thread.Sleep(10000);
-
-                //string result = "";
-                string result = p.StandardOutput.ReadToEnd();
-                string error = p.StandardError.ReadToEnd();
-                return result + "ГРЕШКИ" + error;
-            }
-            catch (Exception ex)
-            {
-                string json = JsonConvert.SerializeObject(ex, Formatting.Indented);
-                return json;
-            }
-        }
-
-        public string Ports(string ip = "8.8.8.8")
-        {
-            try
-            {
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer 
-                // connected to the same address as specified by the server, port
-                // combination.
-                Int32 port = 53;
-                TcpClient client = new TcpClient(ip, port);
-
-                string message = "test";
-
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
-
-                NetworkStream stream = client.GetStream();
-
-                // Send the message to the connected TcpServer. 
-                stream.Write(data, 0, data.Length);
-
-                Console.WriteLine("Sent: {0}", message);
-
-                // Receive the TcpServer.response.
-
-                // Buffer to store the response bytes.
-                data = new Byte[256];
-
-                // String to store the response ASCII representation.
-                String responseData = String.Empty;
-
-                // Read the first batch of the TcpServer response bytes.
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);
-
-                // Close everything.
-                stream.Close();
-                client.Close();
-            }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine("ArgumentNullException: {0}", e);
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            return "";
-
-
-        }
-    }
-
-    public class MyPingReply
-    {
-        //
-        // Summary:
-        //     Gets the address of the host that sends the Internet Control Message Protocol
-        //     (ICMP) echo reply.
-        //
-        // Returns:
-        //     An System.Net.IPAddress containing the destination for the ICMP echo message.
-        public string Address { get; set; }
-        //
-        // Summary:
-        //     Gets the buffer of data received in an Internet Control Message Protocol (ICMP)
-        //     echo reply message.
-        //
-        // Returns:
-        //     A System.Byte array containing the data received in an ICMP echo reply message,
-        //     or an empty array, if no reply was received.
-        public byte[] Buffer { get; set; }
-        //
-        // Summary:
-        //     Gets the options used to transmit the reply to an Internet Control Message Protocol
-        //     (ICMP) echo request.
-        //
-        // Returns:
-        //     A System.Net.NetworkInformation.PingOptions object that contains the Time to
-        //     Live (TTL) and the fragmentation directive used for transmitting the reply if
-        //     System.Net.NetworkInformation.PingReply.Status is System.Net.NetworkInformation.IPStatus.Success;
-        //     otherwise, null.
-        public PingOptions Options { get; set; }
-        //
-        // Summary:
-        //     Gets the number of milliseconds taken to send an Internet Control Message Protocol
-        //     (ICMP) echo request and receive the corresponding ICMP echo reply message.
-        //
-        // Returns:
-        //     An System.Int64 that specifies the round trip time, in milliseconds.
-        public long RoundtripTime { get; set; }
-        //
-        // Summary:
-        //     Gets the status of an attempt to send an Internet Control Message Protocol (ICMP)
-        //     echo request and receive the corresponding ICMP echo reply message.
-        //
-        // Returns:
-        //     An System.Net.NetworkInformation.IPStatus value indicating the result of the
-        //     request.
-        public IPStatus Status { get; set; }
     }
 }
