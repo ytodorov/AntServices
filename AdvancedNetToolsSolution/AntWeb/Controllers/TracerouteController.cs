1f637d5c22bd1dc2736966be221568a169f86b31
ytodorov <ytodorov@ytodorov.com>
2016-03-20 21:31:48 +0200

    UI

--- AdvancedNetToolsSolution/AntWeb/Controllers/TracerouteController.cs
+++ AdvancedNetToolsSolution/AntWeb/Controllers/TracerouteController.cs
@@ -14,7 +14,7 @@ namespace SmartAdminMvc.Controllers
         public ActionResult Index()
         {
             using (HttpClient client = new HttpClient())
-            {
+            {                
                 string url = "http://antnortheu.cloudapp.net/home/exec?program=tracert&args=8.8.8.8%20-q%20-i%200%20-n%2010";
                 var res = client.GetStringAsync(url).Result;
 
