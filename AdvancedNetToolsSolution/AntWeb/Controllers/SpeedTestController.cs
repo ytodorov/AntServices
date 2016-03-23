cc3650a928ae7054bf8f415d529e42c9d044b2a1
ytodorov <ytodorov@ytodorov.com>
2016-03-23 21:45:00 +0200

    speedtest

---
+++ AdvancedNetToolsSolution/AntWeb/Controllers/SpeedTestController.cs
@@ -0,0 +1,44 @@
+﻿#region Using
+
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using SmartAdminMvc.Infrastructure;
+using SmartAdminMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+#endregion
+
+namespace SmartAdminMvc.Controllers
+{
+
+    public class SpeedtestController : BaseController
+    {
+        // GET: home/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Download(int bytes)
+        {
+            Response.ContentType = "text/plain; charset=utf-8";
+            Request.Headers["Accept-Encoding"] = "";
+            //string s = new string('A', bytes);
+            //return s;
+
+            RNGCryptoServiceProvider Rand = new RNGCryptoServiceProvider();
+            byte[] byteArray = new byte[bytes];
+            Rand.GetBytes(byteArray);
+            string s = Convert.ToBase64String(byteArray);
+            return s;
+        }
+
+
+    }
+}
