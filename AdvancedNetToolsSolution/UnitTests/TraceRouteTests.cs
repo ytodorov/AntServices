792c8ecaf18344b05850c6441ac9f76849399cda
ytodorov <ytodorov@ytodorov.com>
2016-03-27 15:19:12 +0300

    Created TraceRouteTests

---
+++ AdvancedNetToolsSolution/UnitTests/TraceRouteTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests
+{
+    public class TraceRouteTests
+    {
+        [Fact]
+        public void GetTraceRouteString()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var encodedArgs = Uri.EscapeDataString($"--traceroute 92.247.12.80 -sn -T5");
+                string url = "http://antnortheu.cloudapp.net/home/exec?program=nmap&args=" + encodedArgs;
+                var res = client.GetStringAsync(url).Result;
+            }
+        }
+    }
+}
