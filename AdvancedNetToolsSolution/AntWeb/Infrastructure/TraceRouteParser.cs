3876ccb8a4b0c15bb42a0e06e7846c086483097b
ivalexandar <alexandar@ivalexandar.com>
2016-03-29 16:13:00 +0300

    fixed code for the first line "1     .... 4"

--- AdvancedNetToolsSolution/AntWeb/Infrastructure/TraceRouteParser.cs
+++ AdvancedNetToolsSolution/AntWeb/Infrastructure/TraceRouteParser.cs
@@ -26,13 +26,14 @@ namespace SmartAdminMvc.Infrastructure
         public static TraceRouteReplyViewModel ParseSingleLine(string line)
         {
             TraceRouteReplyViewModel result = new TraceRouteReplyViewModel();
-            var times = line.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
             int hop;
-            int.TryParse(times[0], out hop);
             double rtt;
-            string address;
-            string ip;
-            if (!string.IsNullOrEmpty(times[4]))
+            string address, ip;
+
+            var times = line.Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+            int.TryParse(times[0], out hop);
+            
+            if (times.Length == 4)
             {
                 double.TryParse(times[1], NumberStyles.Any, CultureInfo.InvariantCulture, out rtt);
                 if (!string.IsNullOrEmpty(times[3]))
@@ -45,17 +46,17 @@ namespace SmartAdminMvc.Infrastructure
                     address = "";
                     ip = times[3];
                 }
+                ip = ip.Substring(1, ip.Length - 2);
             }
             else
             {
-                double.TryParse(times[2], out rtt);
+                double.TryParse(times[0], out rtt);
                 address = "";
                 ip = "";
             }
             result.Hop = hop;
             result.Rtt = rtt;
             result.AddressName = address;
-            ip = ip.Substring(1, ip.Length - 2);
             result.Ip = ip;
 
             return result;
