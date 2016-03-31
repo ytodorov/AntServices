047bf099552f28af5f91195eaf7babe5b1c16973
ivalexandar <alexandar@ivalexandar.com>
2016-03-31 17:34:11 +0300

    added seleniumUnitTests Project

---
+++ AdvancedNetToolsSolution/UnitTestProject1/PingUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace seleniumUnitTests
+{
+    [TestClass]
+    public class PingUnitTest
+    {
+        static IWebDriver driverGC;
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {
+            driverGC = new ChromeDriver(@"C:\chomedriver_win32");
+        }
+
+        public static void main(String[] args)
+        {
+            driverGC.Navigate().GoToUrl("http://localhost:56110/ping");
+            driverGC.FindElement(By.Id("ip")).SendKeys("www.abv.bg");
+            driverGC.FindElement(By.Id("btnPing")).SendKeys(Keys.Enter);
+
+        }
+    }
+}
