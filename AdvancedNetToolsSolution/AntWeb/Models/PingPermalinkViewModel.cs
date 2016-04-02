c35d3f538228941cd71d153f21af048b0202a5a2
ytodorov <ytodorov@ytodorov.com>
2016-04-02 13:53:19 +0300

    Test for permalinks. Changed to .net 4.6.1

---
+++ AdvancedNetToolsSolution/AntWeb/Models/PingPermalinkViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartAdminMvc.Models
+{
+    public class PingPermalinkViewModel
+    {
+        public string Ip { get; set; }
+
+        public List<PingReplySummaryViewModel> PingReplies { get; set; }
+    }
+}
