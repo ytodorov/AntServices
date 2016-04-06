ac85fb5261da40853d3f684fe1ae790663bd716e
ytodorov <ytodorov@ytodorov.com>
2016-04-06 15:26:33 +0300

    cannot reset changes

--- AdvancedNetToolsSolution/Homer_MVC/Controllers/TracerouteController.cs
+++ AdvancedNetToolsSolution/Homer_MVC/Controllers/TracerouteController.cs
@@ -1,59 +1,92 @@
-﻿#region Using
-
-using Kendo.Mvc.Extensions;
-using Kendo.Mvc.UI;
-using SmartAdminMvc.Extensions;
-using SmartAdminMvc.Infrastructure;
-using SmartAdminMvc.Models;
+﻿using Homer_MVC.Infrastructure;
+using Homer_MVC.Models;
+using Microsoft.AspNet.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
-using System.Runtime.Caching;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 
-#endregion
-
-namespace SmartAdminMvc.Controllers
+namespace Homer_MVC.Controllers
 {
-
-    public class TracerouteController : BaseController
+    public class TraceRouteController : BaseController
     {
-        // GET: home/index
-        public ActionResult Index(int? id)
+        public ActionResult Index()
         {
-            if (id == 123)
-            {
-               var result =  MemoryCache.Default.Get("123") as string;
-                return View(model: result);
-            }
-            return View();
+            //if (id.HasValue)
+            //{
+            //    using (AntDbContext context = new AntDbContext())
+            //    {
+            //        PingPermalink pp = context.PingPermalinks.Find(id);
+            //        if (pp != null)
+            //        {
+
+            //            PingPermalinkViewModel ppvm = new PingPermalinkViewModel();
+            //            ppvm.Ip = pp.DestinationAddress;
+            //            ppvm.Id = pp.Id;
+            //            ppvm.PingResponseSummaries = AutoMapper.Mapper.DynamicMap<List<PingResponseSummaryViewModel>>(pp.PingResponseSummaries);
+
+            //            ppvm.GoogleMapString = Utils.GetGoogleMapsString(new string[] { Constants.DublinUrl, ppvm.PingResponseSummaries[0].SourceAddress });
+            //            return View(model: ppvm);
+            //        }
+            //    }
+
+            //}
+            return View(model: new TraceRouteReplyViewModel());
+
         }
-
-        public ActionResult Read([DataSourceRequest] DataSourceRequest request, string ip)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                var encodedArgs = Uri.EscapeDataString($" --traceroute -sn -n {ip}");
-                string url = "http://antnortheu.cloudapp.net/home/exec?program=nmap&args=" + encodedArgs;
-                var tracerouteSummary = client.GetStringAsync(url).Result;
-
-                List<TraceRouteReplyViewModel> list = TraceRouteParser.ParseSummary(tracerouteSummary);
-
-                var dsResult = Json(list.ToDataSourceResult(request));
-                return dsResult;
-            }
-        }
-
         [HttpPost]
-        public ActionResult SaveResultHtml(string resultHtmlBase64)
+        public ActionResult GenerateId(TraceRouteReplyViewModel trrvm)
         {
-            string resultHtml = resultHtmlBase64.Base64Decode();
+            if (string.IsNullOrEmpty(trrvm.Ip))
+            {
+                return Json(string.Empty);
+            }
+            List<TraceRouteReplyViewModel> list = new List<TraceRouteReplyViewModel>();
+            List<Task<string>> tasks = new List<Task<string>>();
+            List<HttpClient> clients = new List<HttpClient>();
 
-            MemoryCache.Default.Add("123", resultHtml, null);
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    HttpClient client = new HttpClient();
+                    clients.Add(client);
+                    var encodedArgs = Uri.EscapeDataString($"");
+                    string url = "http://antnortheu.cloudapp.net/home/exec?program=nping&args=" + encodedArgs;
+                    Task<string> task = client.GetStringAsync(url);
+                    var summary = TraceRouteParser.ParseSummary(task.Result);
+                    list.Add(summary[0]);
+                    tasks.Add(task);
+                }
 
-            return new EmptyResult();
+
+                Task.WaitAll(tasks.ToArray());
+
+                for (int i = 0; i < tasks.Count; i++)
+                {
+                    var summary = TraceRouteParser.ParseSummary(tasks[i].Result);
+                    //summary.SourceAddress = "antnortheu.cloudapp.net"; // TEEEEEMP
+                    list.Add(summary[0]);
+                    clients[i].Dispose();
+                }
+            }
+
+            // Save to Db
+
+            //using (AntDbContext context = new AntDbContext())
+            //{
+            //    PingPermalink pp = new PingPermalink();                
+            //    pp.DestinationAddress = prvm.Ip;
+
+            //    PingResponseSummary pr = AutoMapper.Mapper.DynamicMap<PingResponseSummary>(list[0]);
+
+            //    pp.PingResponseSummaries.Add(pr);
+            //    context.PingPermalinks.Add(pp);
+            //    context.SaveChanges();
+
+            //    return Json(pp.Id);
+            //}
+            return Json(1);
         }
-
     }
 }
