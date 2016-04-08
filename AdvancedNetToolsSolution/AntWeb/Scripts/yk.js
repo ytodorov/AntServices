5d34b569d0f8b26bc6538485de3ac4cc9a579ce7
ytodorov <ytodorov@ytodorov.com>
2016-04-08 23:11:37 +0300

    fix build

--- AdvancedNetToolsSolution/AntWeb/Scripts/yk.js
+++ AdvancedNetToolsSolution/AntWeb/Scripts/yk.js
@@ -1,3 +1,13 @@
 ﻿$(window).resize(function () {
     $(".k-chart").data("kendoChart").refresh();
+   
 });
+
+$(window).ready(function myfunction() {
+    //заради менюто което се премахва на малък екран   
+    setTimeout(function resizeChart() {
+        debugger;
+        $(".k-chart").data("kendoChart").refresh();
+    }
+   , 1);
+})
