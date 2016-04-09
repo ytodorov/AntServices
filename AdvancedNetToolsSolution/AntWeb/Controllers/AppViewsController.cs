094ce2fc87cf45e98b940179b439d3a9de623cb2
ivalexandar <alexandar@ivalexandar.com>
2016-04-09 10:47:31 +0300

    changes

---
+++ AdvancedNetToolsSolution/AntWeb/Controllers/AppViewsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homer_MVC.Controllers
+{
+    public class AppViewsController : Controller
+    {
+        public ActionResult EmailCompose()
+        {
+            return View();
+        }
+    }
+}
---
+++ AdvancedNetToolsSolution/UnitTests/SendMailTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests
+{
+    public class SendMailTests
+    {
+        [Fact]
+        public void SendMailTest()
+        {
+
+            try
+            {
+                MailMessage mailMsg = new MailMessage();
+
+                // To
+                mailMsg.To.Add(new MailAddress("ytodorov@ytodorov.com", "Dancho"));
+
+                // From
+                mailMsg.From = new MailAddress("ivanov.alexandar.bg@gmail.bg", "Alex");
+
+                // Subject and multipart/alternative Body
+                mailMsg.Subject = "Test";
+                string text = "text body";
+                string html = @"<p>html body</p>";
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+
+                // Init SmtpClient and send
+                SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+                System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("alexandariv", "Palec!%!0");
+                smtpClient.Credentials = credentials;
+
+                smtpClient.Send(mailMsg);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
