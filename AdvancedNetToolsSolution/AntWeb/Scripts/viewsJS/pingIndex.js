959d2f119539592587424e89c3986ae58df233e8
ytodorov <ytodorov@ytodorov.com>
2016-04-10 00:01:49 +0300

    added missing folders images and icons

---
+++ AdvancedNetToolsSolution/AntWeb/Scripts/viewsJS/pingIndex.js
@@ -0,0 +1,33 @@
+﻿$("#btnPing").click(function btnPingClick(e) {
+    e.preventDefault();
+
+    var l = $("#btnPing").ladda();
+
+    // Start loading
+    l.ladda('start');
+
+
+    $.ajax({
+        method: "POST",
+        url: "/ping/GenerateId",
+        data: {
+            ip: $("#ip").val(),
+            packetsCount: $("#tbPacketsCount").val(),
+            packetsSize: $("#tbPacketsSize").val(),
+            delayBetweenPings: $("#tbDelayBetweenPings").val(),
+            ttl: $("#tbTTL").val(),
+            df: $('#cbDontFragment').is(':checked')
+        },
+        success: function f(id) {
+            window.location.href = "ping?id=" + id;
+
+        }
+    })
+
+});
+$("#ip").keyup(function myfunction(eventData) {
+    if (eventData.keyCode == 13) {
+        $("#btnPing").click();
+    }
+
+});
