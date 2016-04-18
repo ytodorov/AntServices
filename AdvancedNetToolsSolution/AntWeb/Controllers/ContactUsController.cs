423b45512709b501ec4bc640d039166bc8162921
ytodorov <ytodorov@ytodorov.com>
2016-04-18 23:42:16 +0300

    fix mail, added FAQ section

--- AdvancedNetToolsSolution/AntWeb/Controllers/ContactUsController.cs
+++ AdvancedNetToolsSolution/AntWeb/Controllers/ContactUsController.cs
@@ -14,25 +14,28 @@ namespace Homer_MVC.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult SendEmail(string name, string email, string title, string emailBody)
+        public ActionResult SendEmail(string senderName, string senderMail, string subject, string emailBody)
         {
-            if (name != "" && email != "" && emailBody != "")
+            if (senderName != "" && senderMail != "" && emailBody != "")
             {
                 MailMessage mailMsg = new MailMessage();
 
                 // To
-                mailMsg.To.Add(new MailAddress("alex_i_bg@mail.bg", "Alex"));
+                mailMsg.To.Add(new MailAddress("tfn@ytodorov.com", "YT"));
                 // From
-                mailMsg.From = new MailAddress(email, name);
+                mailMsg.From = new MailAddress(senderMail, senderName);
 
                 // Subject and multipart/alternative Body
-                mailMsg.Subject = title;
+                mailMsg.Subject = subject;
                 mailMsg.Body = emailBody;
 
                 // Init SmtpClient and send
                 SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
-                System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("alexxokey", "1510alex");
+                //smtpClient.EnableSsl = true;
+                System.Net.NetworkCredential credentials = new System.Net.NetworkCredential("ytodorov@ytodorov.com", "17");
                 smtpClient.Credentials = credentials;
+
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
 
                 smtpClient.Send(mailMsg);
             }
