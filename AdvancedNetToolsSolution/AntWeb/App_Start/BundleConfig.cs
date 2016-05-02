82f926032b56ea433983b28cde2227437631a4ed
ytodorov <ytodorov@ytodorov.com>
2016-05-03 00:41:13 +0300

    <clientCache cacheControlMode="UseMaxAge" cacheControlMaxAge="365.00:00:00" />

--- AdvancedNetToolsSolution/AntWeb/App_Start/BundleConfig.cs
+++ AdvancedNetToolsSolution/AntWeb/App_Start/BundleConfig.cs
@@ -14,60 +14,57 @@ namespace SmartAdminMvc
 
             // Homer script
             bundles.Add(new ScriptBundle(virtualPath: "~/bundles/homer/js").Include(
-                    virtualPaths: new string[] { "~/Vendor/metisMenu/dist/metisMenu.min.js",
-                        //"~/Vendor/iCheck/icheck.min.js",
+                    virtualPaths: new string[] { "~/vendor/metisMenu/dist/metismenu.min.js",
+                        //"~/vendor/iCheck/icheck.min.js",
                         "~/Scripts/homer.js" }));
 
             // Animate.css
             bundles.Add(new StyleBundle(virtualPath: "~/bundles/animate/css").Include(
-                    virtualPath: "~/Vendor/animate.css/animate.min.css"));
+                    virtualPath: "~/vendor/animate.css/animate.min.css"));
 
             // Pe-icon-7-stroke
             bundles.Add(new StyleBundle(virtualPath: "~/bundles/peicon7stroke/css").Include(
-                    virtualPath: "~/Icons/pe-icon-7-stroke/css/pe-icon-7-stroke.css", transforms: new System.Web.Optimization.IItemTransform[] { new CssRewriteUrlTransform() }));
+                    virtualPath: "~/icons/pe-icon-7-stroke/css/pe-icon-7-stroke.css", transforms: new System.Web.Optimization.IItemTransform[] { new CssRewriteUrlTransform() }));
 
             // Font Awesome icons style
             bundles.Add(new StyleBundle(virtualPath: "~/bundles/font-awesome/css").Include(
-                    virtualPath: "~/Vendor/fontawesome/css/font-awesome.min.css",
+                    virtualPath: "~/vendor/fontawesome/css/font-awesome.min.css",
                     transforms: new System.Web.Optimization.IItemTransform[] { new CssRewriteUrlTransform() }));
 
             // Bootstrap style
             bundles.Add(new StyleBundle(virtualPath: "~/bundles/bootstrap/css").Include(
-                    virtualPath: "~/Vendor/bootstrap/dist/css/bootstrap.min.css", transforms: new System.Web.Optimization.IItemTransform[] { new CssRewriteUrlTransform() }));
+                    virtualPath: "~/vendor/bootstrap/dist/css/bootstrap.min.css", transforms: new System.Web.Optimization.IItemTransform[] { new CssRewriteUrlTransform() }));
 
             // Bootstrap
             bundles.Add(new ScriptBundle(virtualPath: "~/bundles/bootstrap/js").Include(
-                    virtualPath: "~/Vendor/bootstrap/dist/js/bootstrap.min.js"));
+                    virtualPath: "~/vendor/bootstrap/dist/js/bootstrap.min.js"));
 
             // Ladda
             bundles.Add(new ScriptBundle(virtualPath: "~/bundles/ladda/js").Include(
-                    virtualPaths: new string[] { "~/Vendor/ladda/dist/spin.min.js",
-                        "~/Vendor/ladda/dist/ladda.min.js",
-                        "~/Vendor/ladda/dist/ladda.jquery.min.js" }));
+                    virtualPaths: new string[] { "~/vendor/ladda/dist/spin.min.js",
+                        "~/vendor/ladda/dist/ladda.min.js",
+                        "~/vendor/ladda/dist/ladda.jquery.min.js" }));
 
             // Ladda style
             bundles.Add(new StyleBundle(virtualPath: "~/bundles/ladda/css").Include(
-                    virtualPath: "~/Vendor/ladda/dist/ladda-themeless.min.css"));
-
-            // jQuery
-            //bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include(
-            //          "~/Vendor/jquery/dist/jquery.min.js"));
+                    virtualPath: "~/vendor/ladda/dist/ladda-themeless.min.css"));
+                
 
             // jQuery UI
             bundles.Add(new ScriptBundle(virtualPath: "~/bundles/jqueryui/js").Include(
-                    virtualPath: "~/Vendor/jquery-ui/jquery-ui.min.js"));
+                    virtualPath: "~/vendor/jquery-ui/jquery-ui.min.js"));
 
             // jQuery Validation
             bundles.Add(new ScriptBundle(virtualPath: "~/bundles/validation/js").Include(
-                    virtualPath: "~/Vendor/jquery-validation/jquery.validate.min.js"));
+                    virtualPath: "~/vendor/jquery-validation/jquery.validate.min.js"));
 
             // Toastr
             bundles.Add(new ScriptBundle(virtualPath: "~/bundles/toastr/js").Include(
-                    virtualPath: "~/Vendor/toastr/build/toastr.min.js"));
+                    virtualPath: "~/vendor/toastr/build/toastr.min.js"));
 
             // Toastr style
             bundles.Add(new StyleBundle(virtualPath: "~/bundles/toastr/css").Include(
-                    virtualPath: "~/Vendor/toastr/build/toastr.min.css"));
+                    virtualPath: "~/vendor/toastr/build/toastr.min.css"));
 
         }
 
