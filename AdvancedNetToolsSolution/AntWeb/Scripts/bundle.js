27408560a117b4f72cd0bca6d5dabb19ea7901bd
ytodorov <ytodorov@ytodorov.com>
2016-05-07 16:55:30 +0300

    test for u/d speed

--- AdvancedNetToolsSolution/AntWeb/Scripts/bundle.js
+++ AdvancedNetToolsSolution/AntWeb/Scripts/bundle.js
@@ -220,7 +220,7 @@ var Ant;
     Ant.Global = Global;
 })(Ant || (Ant = {}));
 window.antGlobal = new Ant.Global();
-
+//# sourceMappingURL=antGlobal.js.map
 $(window).resize(function () {
     //заради менюто което се премахва на малък екран   
     setTimeout(function () {
@@ -454,3 +454,4 @@ $(window).on("error", function (e) {
     debugger;
     window.antGlobal.showNotification(e.originalEvent.error.message, "error");
 });
+//# sourceMappingURL=yk.js.map
