fb10912c17f6a9c0520c97c679f68ed9b71115b5
ytodorov <ytodorov@ytodorov.com>
2016-09-17 17:41:58 +0300

    added word files to process

---
+++ AdvancedNetToolsSolution/AntWeb/Scripts/word/index.js
@@ -0,0 +1,9 @@
+function onSelect(ev) {
+    var file = ev.files[0];
+    //clearHighlights();
+    if (!/.docx|.rtf|.html|.txt/.test(file.extension)) {
+        alert("Only documents with *.docx, *.rtf, *.html or *.txt extensions are accepted!");
+        ev.preventDefault();
+    }
+}
+//# sourceMappingURL=index.js.map
---
+++ AdvancedNetToolsSolution/AntWeb/Scripts/word/index.ts
@@ -0,0 +1,10 @@
+﻿function onSelect(ev) {
+    var file = ev.files[0];
+    //clearHighlights();
+
+    if (!/.docx|.rtf|.html|.txt/.test(file.extension)) {
+        alert("Only documents with *.docx, *.rtf, *.html or *.txt extensions are accepted!");
+        ev.preventDefault();
+    }
+  
+}
