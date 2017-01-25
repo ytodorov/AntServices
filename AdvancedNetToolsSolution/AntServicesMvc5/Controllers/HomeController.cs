46384b108d58a733c2419f0b894cb94440d3f963
ytodorov <ytodorov@ytodorov.com>
2017-01-25 18:57:50 +0200

    uploaded downloadyt

--- AdvancedNetToolsSolution/AntServicesMvc5/Controllers/HomeController.cs
+++ AdvancedNetToolsSolution/AntServicesMvc5/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
-﻿using Newtonsoft.Json;
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using Newtonsoft.Json;
 using SmartAdminMvc.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Web;
 using System.Web.Hosting;
@@ -76,6 +79,47 @@ namespace AntServicesMvc5.Controllers
             }
         }
 
+        public string ConvertAudio(string inputFileBytesAsBase64String, string inputFileExtensionWithDot,
+            string outputFileExtensionWithDot)
+        {
+            var folderPath = Path.GetTempPath();  //server.MapPath("~/tmp");
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            var tempFileInput = Path.Combine(folderPath, "input" + Guid.NewGuid().ToString() + inputFileExtensionWithDot);
+
+            var bytes = Convert.FromBase64String(inputFileBytesAsBase64String);
+
+            System.IO.File.WriteAllBytes(tempFileInput, bytes);
+
+            var tempFileOutput = Path.Combine(folderPath, "output" + Guid.NewGuid().ToString() + outputFileExtensionWithDot);
+            System.IO.File.WriteAllBytes(tempFileOutput, new byte[0]);
+
+            var inputFile = new MediaFile { Filename = tempFileInput };
+            var outputFile = new MediaFile { Filename = tempFileOutput };
+
+            byte[] result = null;
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+                engine.Convert(inputFile, outputFile);
+
+                result = System.IO.File.ReadAllBytes(outputFile.Filename);
+
+            }
+
+            System.IO.File.Delete(tempFileInput);
+            System.IO.File.Delete(tempFileOutput);
+
+            string base64Result = Convert.ToBase64String(result);
+
+            return base64Result;
+        }
+
         public string ExecFfmpeg(string program, string args, string inputFileName, string inputBytesBase64)
         {
             byte[] tempFileBytes = Convert.FromBase64String(inputBytesBase64);
@@ -137,7 +181,7 @@ namespace AntServicesMvc5.Controllers
                                 p.Kill();
                             }
                         }
-                        catch(Exception)
+                        catch (Exception)
                         {
 
                         }
@@ -157,7 +201,7 @@ namespace AntServicesMvc5.Controllers
                         return base64Result;
                     }
 
-                 
+
                 }
                 return "No such program " + program;
             }
@@ -172,9 +216,130 @@ namespace AntServicesMvc5.Controllers
             }
             finally
             {
-                
+
             }
             return string.Empty;
+
+        }
+
+        public ActionResult YoutubeDownload(string program, string args)
+        {
+            Response.ContentType = "text/plain; charset=utf-8";
+            if (!program.EndsWith(value: ".exe"))
+            {
+                program += ".exe";
+            }
+
+            try
+            {
+
+
+                string[] allFiles = Directory.GetFiles(HostingEnvironment.ApplicationPhysicalPath, searchPattern: "*.exe", searchOption: SearchOption.AllDirectories);
+
+                StringBuilder sb = new StringBuilder();
+                foreach (var s in allFiles)
+                {
+                    sb.AppendLine(s);
+                }
+
+
+                string programFullPath = string.Empty; //allFiles.FirstOrDefault(f =>
+                //f.ToLowerInvariant().IndexOf(program.ToLowerInvariant(), 0, StringComparison.InvariantCultureIgnoreCase) >= 0);
+
+                foreach (string fullFileName in allFiles)
+                {
+                    string fn = Path.GetFileName(fullFileName);
+                    if (fn.ToLowerInvariant().Equals(program.ToLowerInvariant(), StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        programFullPath = fullFileName;
+                        sb.AppendLine(fn.ToLowerInvariant() + " IS EQUAL TO " + program.ToLowerInvariant());
+                    }
+                    else
+                    {
+                        sb.AppendLine(fn.ToLowerInvariant() + " is not equal to " + program.ToLowerInvariant());
+                    }
+                }
+
+
+                if (!string.IsNullOrEmpty(programFullPath))
+                {
+                    FileInfo fi = new FileInfo(programFullPath);
+
+                    var allFilesInExeDir = fi.Directory.GetFiles();
+
+
+                    var guid = Guid.NewGuid().ToString();
+                    var p = new Process();
+                    p.StartInfo.FileName = programFullPath;
+                    p.StartInfo.Arguments = args + " -o " + guid + ".tmp";
+                    p.StartInfo.RedirectStandardInput = true;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardError = true;
+                    p.StartInfo.UseShellExecute = false;
+
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    p.StartInfo.ErrorDialog = false;
+                    p.StartInfo.WorkingDirectory = fi.Directory.FullName;
+
+
+                    p.Start();
+                    p.WaitForExit(milliseconds: 1000000);
+
+
+
+                    allFiles = Directory.GetFiles(HostingEnvironment.ApplicationPhysicalPath, searchPattern: "*.*", searchOption: SearchOption.AllDirectories);
+                    string resultPath = allFiles.FirstOrDefault(f => Path.GetFileName(f).ToLower().Contains(guid.ToLower()));
+
+                    string result = p.StandardOutput.ReadToEnd();
+                    string error = p.StandardError.ReadToEnd();
+
+                    //return result + "ГРЕШКИ" + error;
+                    allFilesInExeDir = fi.Directory.GetFiles();
+
+                    if (System.IO.File.Exists(resultPath))
+                    {
+                        try
+                        {
+                            if (!p.HasExited)
+                            {
+                                p.Kill();
+                            }
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                        var bytesToReturs = System.IO.File.ReadAllBytes(resultPath);
+                        //var base64Result = Convert.ToBase64String(bytesToReturs);
+
+                        //Thread.Sleep(500);
+                        try
+                        {
+                            System.IO.File.Delete(resultPath);
+                        }
+                        catch
+                        {
+
+                        }
+                        return File(bytesToReturs, "text/html");
+                    }
+                    return  Json("No such output directory " + resultPath + result + error + "search locations: " + sb.ToString());
+
+
+
+                }
+                return Json("No such program " + program + "search locations: " + sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                string json = JsonConvert.SerializeObject(ex, Formatting.Indented);
+                return Json(json);
+            }
+            finally
+            {
+
+            }
 
         }
     }
