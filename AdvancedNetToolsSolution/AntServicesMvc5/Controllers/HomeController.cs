35270f6ba610326a2ed9072a97de53290f6ddfda
ytodorov <ytodorov@ytodorov.com>
2017-01-27 22:53:54 +0200

    pubnub

--- AdvancedNetToolsSolution/AntServicesMvc5/Controllers/HomeController.cs
+++ AdvancedNetToolsSolution/AntServicesMvc5/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using MediaToolkit;
 using MediaToolkit.Model;
 using Newtonsoft.Json;
+using PubNubMessaging.Core;
 using SmartAdminMvc.Infrastructure;
 using System;
 using System.Collections.Generic;
@@ -80,150 +81,53 @@ namespace AntServicesMvc5.Controllers
             }
         }
 
-        public string ConvertAudio(string inputFileBytesAsBase64String, string inputFileExtensionWithDot,
-            string outputFileExtensionWithDot)
+        public void RemoveOldFiles(string programFullPath)
         {
-            var folderPath = Path.GetTempPath();  //server.MapPath("~/tmp");
-
-            if (!Directory.Exists(folderPath))
+            FileInfo di = new FileInfo(programFullPath);
+            var files = di.Directory.GetFiles();
+            foreach (var fileInfo in files)
             {
-                Directory.CreateDirectory(folderPath);
-            }
-
-            var tempFileInput = Path.Combine(folderPath, "input" + Guid.NewGuid().ToString() + inputFileExtensionWithDot);
-
-            var bytes = Convert.FromBase64String(inputFileBytesAsBase64String);
-
-            System.IO.File.WriteAllBytes(tempFileInput, bytes);
-
-            var tempFileOutput = Path.Combine(folderPath, "output" + Guid.NewGuid().ToString() + outputFileExtensionWithDot);
-            System.IO.File.WriteAllBytes(tempFileOutput, new byte[0]);
-
-            var inputFile = new MediaFile { Filename = tempFileInput };
-            var outputFile = new MediaFile { Filename = tempFileOutput };
-
-            byte[] result = null;
-            using (var engine = new Engine())
-            {
-                engine.GetMetadata(inputFile);
-
-                engine.Convert(inputFile, outputFile);
-
-                result = System.IO.File.ReadAllBytes(outputFile.Filename);
-
-            }
-
-            System.IO.File.Delete(tempFileInput);
-            System.IO.File.Delete(tempFileOutput);
-
-            string base64Result = Convert.ToBase64String(result);
-
-            return base64Result;
-        }
-
-        public string ExecFfmpeg(string program, string args, string inputFileName, string inputBytesBase64)
-        {
-            byte[] tempFileBytes = Convert.FromBase64String(inputBytesBase64);
-            Response.ContentType = "text/plain; charset=utf-8";
-            if (!program.EndsWith(value: ".exe"))
-            {
-                program += ".exe";
-            }
-            string pathToWriteTemp = string.Empty;
-            try
-            {
-
-
-                string[] allFiles = Directory.GetFiles(HostingEnvironment.ApplicationPhysicalPath, searchPattern: "*.*", searchOption: SearchOption.AllDirectories);
-                string programFullPath = allFiles.FirstOrDefault(f => Path.GetFileName(f).ToLower().Equals(program.ToLower()));
-                if (!string.IsNullOrEmpty(programFullPath))
+                try
                 {
-                    FileInfo fi = new FileInfo(programFullPath);
-                    pathToWriteTemp = Path.Combine(fi.Directory.FullName, inputFileName);
-
-                    System.IO.File.WriteAllBytes(pathToWriteTemp, tempFileBytes);
-
-                    var allFilesInExeDir = fi.Directory.GetFiles();
-
-
-
-                    var p = new Process();
-                    p.StartInfo.FileName = programFullPath;
-                    p.StartInfo.Arguments = args;
-                    p.StartInfo.RedirectStandardInput = true;
-                    p.StartInfo.RedirectStandardOutput = true;
-                    p.StartInfo.RedirectStandardError = true;
-                    p.StartInfo.UseShellExecute = false;
-
-                    p.StartInfo.CreateNoWindow = true;
-                    p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                    p.StartInfo.ErrorDialog = false;
-                    p.StartInfo.WorkingDirectory = fi.Directory.FullName;
-
-
-                    p.Start();
-                    p.WaitForExit(milliseconds: 10000);
-
-
-                    string resultPath = Path.Combine(fi.Directory.FullName, "output.mp3");
-
-                    //string result = p.StandardOutput.ReadToEnd();
-                    //string error = p.StandardError.ReadToEnd();
-
-                    //return result + "ГРЕШКИ" + error;
-                    allFilesInExeDir = fi.Directory.GetFiles();
-
-                    if (System.IO.File.Exists(resultPath))
+                    var extension = Path.GetExtension(fileInfo.FullName);
+                    if (!extension.Contains("exe"))
                     {
-                        try
+                        if (fileInfo.CreationTimeUtc < DateTime.UtcNow.AddHours(-3))
                         {
-                            if (!p.HasExited)
-                            {
-                                p.Kill();
-                            }
+                            fileInfo.Delete();
                         }
-                        catch (Exception)
-                        {
-
-                        }
-                        var bytesToReturs = System.IO.File.ReadAllBytes(resultPath);
-                        var base64Result = Convert.ToBase64String(bytesToReturs);
-
-                        //Thread.Sleep(500);
-                        try
-                        {
-                            System.IO.File.Delete(pathToWriteTemp);
-                            System.IO.File.Delete(resultPath);
-                        }
-                        catch
-                        {
-
-                        }
-                        return base64Result;
                     }
-
-
                 }
-                return "No such program " + program;
-            }
-            catch (Exception ex)
-            {
-                if (System.IO.File.Exists(pathToWriteTemp))
+                catch(Exception)
                 {
 
-                    string json = JsonConvert.SerializeObject(ex, Formatting.Indented);
-                    return json;
                 }
             }
-            finally
-            {
 
-            }
-            return string.Empty;
-
+            
         }
 
-        public ActionResult YoutubeDownload(string program, string args)
+        /// <summary>
+        /// Download Youtube Audio Video
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        public ActionResult DAV(string guid)
+        {
+            string[] allFiles = Directory.GetFiles(HostingEnvironment.ApplicationPhysicalPath, searchPattern: "*.*", searchOption: SearchOption.AllDirectories);
+
+            var resultPath = allFiles.FirstOrDefault(f => f.Contains(guid));
+
+            string ct = MimeMapping.GetMimeMapping(resultPath);
+
+            FileInfo fi = new FileInfo(resultPath);
+            string fileName = fi.Name.Replace(guid, string.Empty);
+
+            var result = File(resultPath, ct, fileName);
+            return result;
+        }
+
+        public ActionResult YoutubeDownload(string program, string args, string guid, string fileName)
         {
             Response.ContentType = "text/plain; charset=utf-8";
             if (!program.EndsWith(value: ".exe"))
@@ -244,8 +148,7 @@ namespace AntServicesMvc5.Controllers
                 }
 
 
-                string programFullPath = string.Empty; //allFiles.FirstOrDefault(f =>
-                //f.ToLowerInvariant().IndexOf(program.ToLowerInvariant(), 0, StringComparison.InvariantCultureIgnoreCase) >= 0);
+                string programFullPath = string.Empty; 
 
                 foreach (string fullFileName in allFiles)
                 {
@@ -264,40 +167,46 @@ namespace AntServicesMvc5.Controllers
 
                 if (!string.IsNullOrEmpty(programFullPath))
                 {
+                    Task.Factory.StartNew(() => RemoveOldFiles(programFullPath));
+                    
+
                     FileInfo fi = new FileInfo(programFullPath);
 
                     var allFilesInExeDir = fi.Directory.GetFiles();
 
-
-                    var guid = Guid.NewGuid().ToString();
+                    if (string.IsNullOrEmpty(guid))
+                    {
+                        guid = Guid.NewGuid().ToString();
+                    }
                     var p = new Process();
                     p.StartInfo.FileName = programFullPath;
-                    p.StartInfo.Arguments = args + " -o " + guid + ".tmp";
+                    p.StartInfo.Arguments = args + " -o " + guid;
                     p.StartInfo.RedirectStandardInput = true;
                     p.StartInfo.RedirectStandardOutput = true;
                     p.StartInfo.RedirectStandardError = true;
                     p.StartInfo.UseShellExecute = false; // zaradi  p.StartInfo.RedirectStandardInput = true;
-
 
                     p.StartInfo.CreateNoWindow = false;
                     p.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
                     p.StartInfo.ErrorDialog = false;
                     p.StartInfo.WorkingDirectory = fi.Directory.FullName;
 
+                 
 
                     p.Start();
 
                     //https://msdn.microsoft.com/en-us/library/system.diagnostics.process.standarderror(v=vs.110).aspx
-                    string result = p.StandardOutput.ReadToEnd();
+                    //p.StandardOutput.ReadLineAsync(); // .ReadToEnd();
+
+                    while (!p.StandardOutput.EndOfStream)
+                    {
+                        string line = p.StandardOutput.ReadLine();
+                        // do something with line
+                    }
+
                     string error = p.StandardError.ReadToEnd();
 
                     p.WaitForExit(milliseconds: (int)TimeSpan.FromSeconds(1).TotalMilliseconds);
-
-
-
-                   
-
-                  
 
                     allFiles = Directory.GetFiles(HostingEnvironment.ApplicationPhysicalPath, searchPattern: "*.*", searchOption: SearchOption.AllDirectories);
                     string resultPath = allFiles.FirstOrDefault(f => Path.GetFileName(f).ToLower().Contains(guid.ToLower()));
@@ -318,39 +227,18 @@ namespace AntServicesMvc5.Controllers
                         {
 
                         }
+                        FileInfo resultFi = new FileInfo(resultPath);
+                        string filepathWithoutExtension = resultPath.Replace(Path.GetExtension(resultPath), string.Empty);
+                        resultFi.MoveTo(filepathWithoutExtension + fileName);
 
-                        //var bytesToReturs = System.IO.File.ReadAllBytes(resultPath);
+                        Pubnub pubnub = new Pubnub("pub-c-5bd3c97d-e760-4aa8-9b91-0746c78606f9",
+            "sub-c-406da20e-e48e-11e6-b325-02ee2ddab7fe", "sec-c-MzkzZmE0Y2UtODRkMC00MzcxLThmMTYtNWIzOGQyOTVmYjgz");
 
-                        byte[] bytesToReturs = null;
+                        pubnub.Publish(guid, guid, UserCallBack, PubnubClientError);
 
-
-                       
-
-
-                        //using (var file = new FileStream(resultPath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, false))
-                        {
-                            //bytesToReturs = new byte[file.Length];
-                            var file = new FileStream(resultPath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, false);
-                            var test = new FileStreamResult(file, "application/octet-stream");
-                            return test;
-
-                            //int bytesRead = await file.ReadAsync(bytesToReturs, 0, (int)file.Length);
-
-
-                          
-                        }
-                        try
-                        {
-                            System.IO.File.Delete(resultPath);
-                        }
-                        catch
-                        {
-
-                        }
-                        var resToReturn = File(bytesToReturs, "text/html");
-                        return resToReturn;
+                        return new EmptyResult();
                     }
-                    return  Json("No such output directory " + resultPath + result + error + "search locations: " + sb.ToString());
+                    return Json("No such output directory " + resultPath + error + "search locations: " + sb.ToString());
 
 
 
@@ -366,6 +254,21 @@ namespace AntServicesMvc5.Controllers
             {
 
             }
+
+        }
+
+        private void P_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            string message = e.Data;
+        }
+
+        public void UserCallBack(object o)
+        {
+
+        }
+
+        public void PubnubClientError(PubnubClientError error)
+        {
 
         }
     }
