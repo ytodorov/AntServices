4151737184825a8db51bf44521deaa4c7dbe2b5b
ytodorov <ytodorov@ytodorov.com>
2017-03-19 21:36:16 +0200

    configure application insights

--- AdvancedNetToolsSolution/AntWeb/App_Start/FilterConfig.cs
+++ AdvancedNetToolsSolution/AntWeb/App_Start/FilterConfig.cs
@@ -10,6 +10,7 @@ namespace SmartAdminMvc
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
             //http://stackoverflow.com/questions/7265193/mvc-problem-with-custom-error-pages
             // http://benfoster.io/blog/aspnet-mvc-custom-error-pages
             //filters.Add(new HandleErrorAttribute() { View = "Error500" });
